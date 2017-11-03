4cd207b8718de9e4e593b4ee8e727c662cb227de
Daniel Munín González <danmunin@gmail.com>
2017-11-03 10:54:30 +0100

    Modified app.js to separate callbacks from routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
 
-app.post('/', function (req, res) {
+app.post('/echo', echo);
+
+app.listen(port);
+
+console.log('Listening on port: ' +  port);
+
+
+function echo(req,res){
     res.send(req.body);
-})
-
-app.listen(3000, function () {
-
-})
+};
